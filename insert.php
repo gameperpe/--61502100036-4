2f2ddbcbf5672e2483316608eff559b2dc2870b1
gameperpe <76504923+gameperpe@users.noreply.github.com>
2021-02-10 10:19:05 +0700

    1.4 update
    1.4 แก้ไขโดยการเพิ่มรายชื่อลูกค้าและรหัสผ่าน

--- insert.php
+++ insert.php
@@ -1,8 +1,8 @@
 <?php
 include('connect.php');
 
-$query =  "INSERT INTO movie (id, nmovie, date)
-VALUES ('{$_POST['id']}', '{$_POST['nmovie']}', '{$_POST['date']}')";
+$query =  "INSERT INTO movie (id, nmovie, date, fname, pin)
+VALUES ('{$_POST['id']}', '{$_POST['nmovie']}', '{$_POST['date']}', '{$_POST['fname']}', '{$_POST['pin']}')";
 $result = mysqli_query($con, $query); 
 if ($result == TRUE) {
     header("Location: show_form.php");
